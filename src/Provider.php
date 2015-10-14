e10b3ceb84796f20dbcf12619345d17c3a55cc1e
chuchvara <taras@chuchvara.co.ua>
2015-10-14 12:32:02 +0300

    Update Provider.php

--- src/Provider.php
+++ src/Provider.php
@@ -43,7 +43,7 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function mapUserToObject(array $user)
     {
-    	$lastName = isset($user['lastName'])?$user['lastName']:'';
+    	$lastName = isset($user['lastName']) ? $user['lastName'] : '';
     	
         return (new User())->setRaw($user)->map([
             'id' => $user['id'], 'nickname' => null,
